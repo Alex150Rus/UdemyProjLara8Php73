fbd2ff2169ded90b41ce7db760433b429fe1ab21
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-10-22 17:12:30 +0300

    partial templates, including templates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,13 +4,8 @@
 
 @section('content')
     @forelse($posts as $key => $post)
-        {{-- @break($key == 2) --}}
-        {{-- @continue($key == 1)--}}
-        @if($loop->even)
-            <div>{{$key}}.{{ $post['title'] }}</div>
-        @else
-            <div style="background-color: silver">{{$key}}.{{ $post['title'] }}</div>
-        @endif
+    {{-- include passes all variables in cycle/context into view. But it is also possible to pass additional vars via, [] --}}
+    @include('posts.partials.post')
     @empty
         <div>No posts found</div>
     @endforelse
