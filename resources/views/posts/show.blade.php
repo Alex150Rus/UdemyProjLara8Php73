7c44fc82ec300cc5d53f1ea9b83c9de2a7325ad5
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-10-22 15:35:34 +0300

    conditional rendering alternatives

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,16 @@
     @elseif(!$post['is_new'])
         <div>Blog post is old! Using elseif</div>
     @endif
+
+    @unless($post['is_new'])
+        <div>It is an old post using unless (runs if condition is false and no alternatives)</div>
+    @endunless
+
     <h1>{{ $post['title'] }}</h1>
     <p>{{ $post['content'] }}</p>
+
+    @isset($post['has_comments'])
+        <div>The post has some comments using isset</div>
+    @endisset
+
 @endsection
