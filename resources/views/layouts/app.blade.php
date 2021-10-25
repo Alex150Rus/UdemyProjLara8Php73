49f269a1f8a97e14cc59d697f70f721f06060775
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-10-26 01:11:31 +0300

    including assets in Views asset(), versioned assets: mix()

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- asset() was used before mix--}}
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <script src="{{mix('js/app.js')}}" defer></script>
     <title>Laravel App - @yield('title')</title>
 </head>
 <body>
