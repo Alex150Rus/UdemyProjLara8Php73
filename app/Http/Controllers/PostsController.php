3e4a49c4b38082fb6521fc2b5259f948132e07e7
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-10-31 17:48:47 +0300

    Lazy load

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class PostsController extends Controller
@@ -16,7 +17,23 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('posts.index', ['posts' => BlogPost::orderBy('created_at', 'desc')->take(5)->get()]);
+        DB::connection()->enableQueryLog();
+
+        //LazyLoad query qty = 1 + qty Of posts
+        $posts = BlogPost::all();
+
+        foreach ($posts as $post) {
+            foreach ($post->comments as $comment) {
+                //query is done
+                echo $comment->content;
+            }
+        }
+        //endLazyload
+
+        dd(DB::getQueryLog());
+
+
+        return view('posts.index', ['posts' => $posts]);
     }
 
     /**
