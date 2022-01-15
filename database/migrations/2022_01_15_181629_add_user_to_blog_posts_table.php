1c6df7cf6068e8743f8f4844b0ab0b3903f801e2
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2022-01-15 21:52:52 +0300

    added new column to blog_posts table, added foreign key, solved fk constrain problem

---
+++ database/migrations/2022_01_15_181629_add_user_to_blog_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserToBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_posts', function (Blueprint $table) {
+            //nullable для случая, если в таблице, на которую ссылаемся есть данные
+            //тип данных должен строго совпадать с родительским (unsigned не забываем)
+            //$table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog_posts', function (Blueprint $table) {
+            $table->dropForeign('user_id');
+            $table->dropColumn('user_id');
+        });
+    }
+}
