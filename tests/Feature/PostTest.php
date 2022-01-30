96aeaabfb2bc647f55a09bc5489d03292df1a61f
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2022-01-30 20:19:07 +0300

    testing soft deleted models

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -115,7 +115,8 @@ class PostTest extends TestCase
             ->assertSessionHas('status');
 
         $this->assertEquals(session('status'), 'Blog post was deleted!');
-        $this->assertDatabaseMissing('blog_posts',  ['title' => 'New title']);
+        //$this->assertDatabaseMissing('blog_posts',  ['title' => 'New title']);
+        $this->assertSoftDeleted('blog_posts',  ['title' => 'New title']);
     }
 
     private function createDummyBlogPost(): BlogPost {
