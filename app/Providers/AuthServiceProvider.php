e4626d7844f18184305b73f13a8d628e9b1b5b8c
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2022-02-09 22:07:59 +0300

    admin_users and overriding permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,5 +32,32 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('delete-post', function($user, $blogPost){
             return $user->id === $blogPost->user_id;
         });
+
+        //will be called before other Gate checks. And should always return true
+        Gate::before(function($user, $ability){
+            if($user->is_admin) {
+                return true;
+            }
+        });
+
+//        //Overriding ability
+//        Gate::before(function($user, $ability){
+//            if($user->is_admin && $ability === 'delete-post') {
+//                return true;
+//            }
+//        });
+//
+//        //Overriding abilities
+//        Gate::before(function($user, $ability){
+//            if($user->is_admin && in_array($ability, ['delete-post', 'update-post'])) {
+//                return true;
+//            }
+//        });
+
+//        Gate::after(function($user, $ability, $result){
+//            if($user->is_admin) {
+//                return true;
+//            }
+//        });
     }
 }
