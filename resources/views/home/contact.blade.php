3df8a1c0af38e72ed0f389d323c712a0ef731aa6
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2022-03-27 17:32:22 +0300

    using middleware to authorize routes

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -5,4 +5,11 @@
 @section('content')
     <h1>Contact</h1>
     <p>Hello this is contact!</p>
+    @can('home.secret')
+        <p>
+            <a href="{{ route('home.secret') }}">
+                Go to special contact details page!
+            </a>
+        </p>
+    @endcan
 @endsection
